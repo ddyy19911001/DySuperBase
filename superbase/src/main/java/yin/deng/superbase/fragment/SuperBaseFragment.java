287623f9d1aaef8ea25a8faa94bb54bcb2cb7d6a
dengyin <584120792@qq.com>
2019-12-26 17:03:21 +0800

    修改为andoridx版本

--- superbase/src/main/java/yin/deng/superbase/fragment/SuperBaseFragment.java
+++ superbase/src/main/java/yin/deng/superbase/fragment/SuperBaseFragment.java
@@ -1,12 +1,12 @@
 package yin.deng.superbase.fragment;
 
-import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 
 
 import yin.deng.superbase.activity.ToastUtil;
